eea7f5fcc4a42a3674663e867a36a05f9caac19d
Alex8lf <alex7lf@gmail.com>
2018-12-22 15:51:50 +0300

    backend files added

---
+++ src/main/java/com/loftideas/loftideas/LoftIdeasApplication.java
@@ -0,0 +1,14 @@
+package com.loftideas.loftideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoftIdeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoftIdeasApplication.class, args);
+	}
+
+}
+
