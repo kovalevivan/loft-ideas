a6d1b46f99631a6b36b3ac7e3d958c185b161ced
Alex8lf <alex7lf@gmail.com>
2018-12-22 18:00:00 +0300

    added config

--- src/main/java/com/loftideas/loftideas/domain/Story.java
+++ src/main/java/com/loftideas/loftideas/domain/Story.java
@@ -3,10 +3,20 @@ package com.loftideas.loftideas.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.Instant;
 
 @Entity
 public class Story {
     @Id
     @GeneratedValue
     private Long id;
+
+//    private Orgaiazation Community;
+
+    private Instant date;
+
+    private String description;
+
+//    private StoryType status;
+
 }
